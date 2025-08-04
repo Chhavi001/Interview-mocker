86ed203e8198f55508da13354a79f0014409ba3e
chhavi <chhavi2544gautam@gmail.com>
2025-08-05 01:10:50 +0530

    Changes:Firebase configurations & Userdata saved successfully using Handlers

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Header from '@/components/header'
+import AuthHandler from '@/handlers/auth-handler'  
 const PublicLayout = () => {
   return (
     <div className="min-h-screen bg-background">
       {/*handler to store the user data*/}
+      <AuthHandler />
       <Header/>
       <main>
         <Outlet/>
